b183a43c581687cdcc1cac8acf6bd7b2b4b0e547
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-04-11 14:43:19 -0500

    fixed error with github

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyAwesomeQuizApp
+# QuizApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-awesome-quiz-app app is running!');
+    expect(page.getTitleText()).toEqual('quiz-app app is running!');
   });
 
   afterEach(async () => {
