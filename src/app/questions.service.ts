195f9df6dfa2609f3c47ec8b61a18f632338a332
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-04-11 14:10:57 -0500

    Revert "added a welcome component and material design"
    This reverts commit edd8133bd17e8eceb64cabd73e46af858a1c809c.

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Quiz, Question } from './quiz.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
-  constructor(private http: HttpClient) { }
 
-  public getQuizzes() {
-    return this.http.get(`./assets/quiz-list.json`).pipe(
-      map((result: any[]) => {
-        return result.map(r => new Quiz(r.label, r.name, r.description, r.fileName));
-      })
-    );
+  constructor(private http: HttpClient) {
   }
 
-  public getQuestions(fileName: string) {
-    return this.http.get(`./assets/${fileName}.json`).pipe(
-      map((result: any[]) => {
-        return result.map(r => new Question(r.label, r.choices));
-      })
-    );
+  public getJSON(fileId: string) {
+    return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
   }
 }
